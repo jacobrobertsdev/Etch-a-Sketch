0b0be43a9c1cbfc8df5ec737dc1e2eff46cec7e7
Jacob Roberts <jacobroberts.dev@gmail.com>
2023-12-19 17:29:24 -0600

    Add final CSS styles and button logic
    - Add fonts for heading and buttons
    - Add body background color and grid box shadow
    - Add button hover and focus styles
    - Complete logic for all buttons / controls
    - Use event delegation for click listeners

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 const container = document.querySelector(".grid-container");
+let buttons = document.querySelectorAll("button");
 
-function createGrid(dimension, color) {
+// Create the grid, set hover color
+
+function createGrid(dimension) {
   container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+
   for (let i = 1; i <= dimension * dimension; i++) {
     let div = document.createElement("div");
     div.style.backgroundColor = "silver";
     container.appendChild(div);
+
     div.addEventListener("mouseover", () => {
-      div.style.backgroundColor = color;
+      div.style.backgroundColor = "black";
     });
   }
 }
 
-document.addEventListener("DOMContentLoaded", createGrid(16, "black"));
+// Reset the grid color
+
+function resetGrid() {
+  let squares = container.querySelectorAll("div");
+
+  for (let square of squares) {
+    square.style.backgroundColor = "silver";
+  }
+}
+
+// Load default grid
+
+document.addEventListener("DOMContentLoaded", createGrid(16));
+
+// Button controls
+
+for (let button of buttons) {
+  button.addEventListener("click", (e) => {
+    resetGrid();
+
+    if (e.target.classList.contains("sixteen")) {
+      createGrid(16);
+    } else if (e.target.classList.contains("thirty-two")) {
+      createGrid(32);
+    } else if (e.target.classList.contains("sixty-four")) {
+      createGrid(64);
+    } else if (e.target.classList.contains("max")) {
+      createGrid(100);
+    }
+  });
+}
