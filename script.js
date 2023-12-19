dbce53261968f6f28caee86b29d349601a4279f2
Jacob Roberts <jacobroberts.dev@gmail.com>
2023-12-19 15:16:48 -0600

    Add header and grid hover functionality

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".grid-container");
 
-function createGrid(dimension) {
+function createGrid(dimension, color) {
   container.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
   for (let i = 1; i <= dimension * dimension; i++) {
     let div = document.createElement("div");
-    div.style.backgroundColor = "gray";
-    div.style.border = "1px solid black";
+    div.style.backgroundColor = "silver";
     container.appendChild(div);
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = color;
+    });
   }
 }
 
-document.addEventListener("DOMContentLoaded", createGrid(16));
+document.addEventListener("DOMContentLoaded", createGrid(16, "black"));
